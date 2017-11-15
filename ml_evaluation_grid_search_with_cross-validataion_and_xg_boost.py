78687d214476eb673058253604b9217635dfc5b7
Milan <Milan.Karunarathne>
2017-11-15 18:48:02 +0530

    Implemented Grid Search with Cross validation and XGBoost to dynamic file splits and random seed

--- ml_evaluation_grid_search_with_cross-validataion_and_xg_boost.py
+++ ml_evaluation_grid_search_with_cross-validataion_and_xg_boost.py
@@ -3,15 +3,13 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.svm import SVR
 from xgboost import XGBRegressor
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 import sklearn.metrics as mx
 from sklearn.model_selection import GridSearchCV
 # from sklearn.model_selection import RandomizedSearchCV
 
-summary_data = 'resources/train/old/wso2apimanagerperformanceresults.csv'
-summary_data_test = 'resources/test/old/wso2apimanagerperformanceresults.csv'
+summary_data = 'resources/wso2apimanagerperformanceresults.csv'
 t_splitter = ","
-
 csv_select_cols = [0, 1, 2, 3, 7, 10, 11, 12, 13]
 x_select_cols = [0, 1, 2, 3]  # select columns to x (features)
 y_select_col_90th_percentile = 5
@@ -19,13 +17,15 @@ y_select_col_95th_percentile = 6
 y_select_col_99th_percentile = 7
 y_select_col_throughput = 8
 y_select_col_latency = 4
-t_size = 0.0  # percentage for testing (test size)
+t_size = 0.30  # percentage for testing (test size)
 n_rows = 117   # total rows
-row_start = 25  # testing rows at start
-r_seed = 42  # seed of random (random seed)
+r_seed = 98 # seed of random (random seed)
 kernel_grid = ['rbf', 'poly', 'linear']
-c_grid = [1E2]
-epsilion_grid = [0.0001]
+c_grid = [1E2, 1E3, 1E4, 1E5, 1E6]
+epsilion_grid = [0.0001, 0.0005, 0.001, 0.005, 0.1, 0.5, 1, 5]
+target = y_select_col_99th_percentile  # change the target as y_select_col_throughput,
+# y_select_col_latency,y_select_col_90th_percentile,
+# y_select_col_95th_percentile,y_select_col_99th_percentile
 
 arr_rmse = np.array([], dtype='float64')
 arr_rmse_test = np.array([], dtype='float64')
@@ -64,7 +64,7 @@ def evaluator(y_train_ev, y_pred_ev):
 
 
 def array_print(array_get):
-    for i in range(0, len(array_get)):
+    for i in range(0, len(array_get)-1):
         print array_get[i]
 
 
@@ -73,50 +73,39 @@ def array_print(array_get):
 data_splits = np.array([], dtype='float64')
 data_splits = data_reader(csv_file=summary_data, total_row=n_rows, thousands_splitter=t_splitter,
                           csv_select_columns=csv_select_cols, x_column_numbers=x_select_cols,
-                          y_column_number=y_select_col_throughput)
+                          y_column_number=target)
 
-data_splits_test = np.array([], dtype='float64')
-data_splits_test = data_reader(csv_file=summary_data_test, total_row=n_rows, thousands_splitter=t_splitter,
-                               csv_select_columns=csv_select_cols, x_column_numbers=x_select_cols,
-                               y_column_number=y_select_col_throughput)
+x_tra, x_tes, y_tra, y_tes = train_test_split(data_splits[0], data_splits[1], test_size=t_size, random_state=r_seed)
 
 # ##################################################################################
 # SVR Grid Search with Cross- Validation
-
 parameters = {'kernel': kernel_grid, 'C': c_grid, 'epsilon': epsilion_grid}
 svr = SVR()
 svr_model = GridSearchCV(svr, parameters)
-svr_model.fit(data_splits[0], data_splits[1])
-y_prd_test = svr_model.predict(data_splits_test[0])  # predict y values by train x_train
+svr_model.fit(x_tra, y_tra)
+y_prd_test = svr_model.predict(x_tes)  # predict y values by train x_train
 
-print '\ntest data - SVR grid CV\n', array_print(data_splits_test[1])
-print '\ntest pred data - SVR grid CV\n', array_print(y_prd_test)
+print '\ntest data\n', array_print(y_tes)
+print '\ntest pred data - svr gird cv\n', array_print(y_prd_test)
 
-y_prd = svr_model.predict(data_splits[0])
-y_prd_test = svr_model.predict(data_splits_test[0])  # predict y values by train x_train
-current_result_array = evaluator(y_train_ev=data_splits[1], y_pred_ev=y_prd)
-arr_rmse_in = current_result_array[0]
-arr_mape_in = current_result_array[1]
-current_result_array_test = evaluator(y_train_ev=data_splits_test[1], y_pred_ev=y_prd_test)
+current_result_array_test = evaluator(y_train_ev=y_tes, y_pred_ev=y_prd_test)
 arr_rmse_in_test = current_result_array_test[0]
 arr_mape_in_test = current_result_array_test[1]
+
 print "\ntest rmse = ", arr_rmse_in_test
 print "test mape = ", arr_mape_in_test
 print "best hyper parameters = ", svr_model.best_params_, "\n\n"
 
-# ############################################################################
+# #################################################################################
 # xgboost regression
 print 'XGBoost - Regression'
 xgb_model = XGBRegressor()  # haven't parameters
-xgb_model.fit(data_splits[0], data_splits[1])
-y_prd_test = xgb_model.predict(data_splits_test[0])  # predict y values by train x_train
-print '\ntest data\n', array_print(data_splits_test[1])
-print '\ntest pred data\n', array_print(y_prd_test)
+xgb_model.fit(x_tra, y_tra)
+y_prd_test = xgb_model.predict(x_tes)  # predict y values by train x_train
+print '\ntest data\n', array_print(y_tes)
+print '\ntest pred data - xgboost\n', array_print(y_prd_test)
 
-current_result_array = evaluator(y_train_ev=data_splits[1], y_pred_ev=y_prd)
-arr_rmse_in_xg = current_result_array[0]
-arr_mape_in_xg = current_result_array[1]
-current_result_array_test = evaluator(y_train_ev=data_splits_test[1], y_pred_ev=y_prd_test)
+current_result_array_test = evaluator(y_train_ev=y_tes, y_pred_ev=y_prd_test)
 arr_rmse_in_test_xg = current_result_array_test[0]
 arr_mape_in_test_xg = current_result_array_test[1]
 
